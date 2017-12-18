c0abbb0b446eac00565930d91f6f9a6eb3b67fa5
Alexander <dodecad@outlook.com>
2017-12-18 16:12:40 +0300

    Basic solution structure

---
+++ CustomTaskScheduler/CustomTaskScheduler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CustomTaskScheduler
+{
+    public class CustomTaskScheduler : TaskScheduler
+    {
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void QueueTask(Task task)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool previouslyQueued)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
